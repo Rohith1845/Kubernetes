32c7a892c3266faf1865443686ddbd21fda86a3e
rohith <krishnamsettyrohith@gmail.com>
2025-12-11 14:24:35 +0530

    devops

--- ekstcl/workstation.sh
+++ ekstcl/workstation.sh
@@ -11,7 +11,7 @@ usermod -aG docker ec2-user
 
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.34.2/2025-11-13/bin/linux/amd64/kubectl
 chmod +x ./kubectl
-mkdir -p $HOME/bin && cp ./kubectl  /usr/local/bin && export PATH=$HOME/bin:$PATH
+mkdir -p $HOME/bin && cp ./kubectl  $HOME/bin/kubectl && export PATH=$HOME/bin:$PATH
 
 ARCH=amd64
 PLATFORM=$(uname -s)_$ARCH
